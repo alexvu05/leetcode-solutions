e1fb39e9a0a619ecd56718464142f3b9140dd2ac
Alex Vu <alexvu5@outlook.com>
2026-09-05 13:13:57 +0700

    Time: 14 ms (80%), Space: 11.2 MB (80%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.go
@@ -0,0 +1,25 @@
+func firstStableIndex(nums []int, k int) int {
+    n := len(nums)
+    // Precompute suffix minimum from right to left
+    suffMin := make([]int, n)
+    suffMin[n-1] = nums[n-1]
+    for i := n - 2; i >= 0; i-- {
+        if nums[i] < suffMin[i+1] {
+            suffMin[i] = nums[i]
+        } else {
+            suffMin[i] = suffMin[i+1]
+        }
+    }
+    // Sweep left to right tracking prefix maximum
+    // instability[i] = prefMax - suffMin[i]
+    prefMax := 0
+    for i, x := range nums {
+        if x > prefMax {
+            prefMax = x
+        }
+        if prefMax-suffMin[i] <= k {
+            return i
+        }
+    }
+    return -1
+}
